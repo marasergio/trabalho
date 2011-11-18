ee924867f5b80d01d2457caea94ad21ff8a01df7
Mara Regina <maramrps@gmail.com>
2011-11-18 01:48:14 -0200

    concluindo entidade

--- src/java/persistencia/entidades/Usuario.java
+++ src/java/persistencia/entidades/Usuario.java
@@ -21,9 +21,44 @@ public class Usuario {
     private String nome;
     @Column(name="usu_email")
     private String email;
+    @Column(name="usu_senha")
     private String senha;
     
     public Usuario(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
 
     
 }
